6b0f5b081d003d494c0e091bbbe1f95284d1d243
gabriel000rc <37663120+gabriel000rc@users.noreply.github.com>
2019-06-28 22:59:17 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#define MAX 4
+int busca(int vetor[],int ini,int fim,int pesq)
+{
+ while(ini<=fim)
+ {
+  int meio;
+  meio=(ini+fim)/2;
+  if(vetor[meio]==pesq)
+  {
+    return meio;
+  }
+  else if (vetor[meio]<pesq)
+  {
+    return busca(vetor,meio+1,fim,pesq);
+  }
+  else
+  {
+   return busca(vetor,ini,meio-1,pesq);
+  }
+ }
+ return -1;
+
+}
+int main (void)
+{
+ int vetor[MAX]={1,4,5,10},fun,x;
+ printf("Informe o valor a ser encontrado: ");
+ scanf("%d",&x);
+ fun = busca(vetor,0,MAX-1,x);
+ printf("%d",fun);
+ printf("\n\n");
+ return 0;
+}
